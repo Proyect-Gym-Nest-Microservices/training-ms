ccadaac4e2101a15af690d181bd420fc7c4f10b4
sgualotuna <sgualotuna@cobiscorp.com>
2024-11-13 16:03:41 -0500

    cambios

--- src/training-plan/training-plan.controller.ts
+++ src/training-plan/training-plan.controller.ts
@@ -5,44 +5,33 @@ import { CreateTrainingPlanDto } from './dto/create-training-plan.dto';
 import { UpdateTrainingPlanDto } from './dto/update-training-plan.dto';
 import { PaginationDto } from 'src/common';
 
-//@Controller('api/training-plan')
 @Controller()
 export class TrainingPlanController {
   constructor(private readonly trainingPlanService: TrainingPlanService) { }
 
   @MessagePattern('create.training.plan')
   createTrainingPlan(@Payload() createTrainingPlanDto: CreateTrainingPlanDto) {
-  //@Post('create')
-  //createTrainingPlan(@Body() createTrainingPlanDto: CreateTrainingPlanDto) {
     return this.trainingPlanService.createTrainingPlan(createTrainingPlanDto);
   }
 
   @MessagePattern('find.all.training.plan')
   findAllTrainingPlan(@Payload() paginationDto: PaginationDto){
-  //@Get('find-all')
-  //findAllTrainingPlan(@Query() paginationDto: PaginationDto) {
     return this.trainingPlanService.findAllTrainingPlan(paginationDto);
   }
 
   @MessagePattern('find.training.plan.by.id')
   findTrainingPlanById(@Payload('id',ParseIntPipe) id: number) {
-  //@Get('find-by-id/:id')
-  //findTrainingPlanById(@Param('id', ParseIntPipe) id: number) {
     return this.trainingPlanService.findTrainingPlanById(id);
   }
 
   @MessagePattern('update.Training.plan')
   updateTrainingPlan(@Payload() payload: {id:number, updateTrainingPlanDto: UpdateTrainingPlanDto }) {
-  //@Patch('update/:id')
-  //updateTrainingPlan(@Param('id', ParseIntPipe) id: number, @Body() updateTrainingPlanDto: UpdateTrainingPlanDto) {
     const { id, updateTrainingPlanDto} = payload;
     return this.trainingPlanService.updateTrainingPlan(id, updateTrainingPlanDto);
   }
 
   @MessagePattern('remove.training.plan')
   removeTrainingPlan(@Payload('id',ParseIntPipe) id: number) {
-  //@Delete('delete/:id')
-  //removeTrainingPlan(@Param('id',ParseIntPipe) id: number) {
     return this.trainingPlanService.removeTrainingPlan(id);
   }
 }
--- src/workout/workout.controller.ts
+++ src/workout/workout.controller.ts
@@ -11,44 +11,32 @@ export class WorkoutController {
 
   @MessagePattern('create.workout')
   createWorkout(@Payload() createWorkoutDto: CreateWorkoutDto) {
-  //@Post('create')
-  //createWorkout(@Body() createWorkoutDto: CreateWorkoutDto) {
     return this.workoutService.createWorkout(createWorkoutDto); 
   }
 
   @MessagePattern('find.all.Workouts')
   findAllWorkouts(@Payload() paginationDto: PaginationDto) {
-  //@Get('find-all')
-  //findAllWorkouts(@Query() paginationDto: PaginationDto){
     return this.workoutService.findAllWorkouts(paginationDto);
   }
 
   @MessagePattern('find.one.workout')
   findWorkoutById(@Payload('id',ParseIntPipe) id: number) {
-  //@Get('find-by-id/:id')
-  //findWorkoutById(@Param('id', ParseIntPipe) id: number) {
     return this.workoutService.findWorkoutById(id);
   }
 
   @MessagePattern('find.one.exercise.in.workout')
   findExerciseInWorkoutById(@Payload('id',ParseIntPipe) id: number) {
-  //@Get('find-by-id-exercise-in-workout/:id')
-  //findExerciseInWorkoutById(@Param('id', ParseIntPipe) id: number) {
     return this.workoutService.findExerciseInWorkoutById(id);
   }
 
   @MessagePattern('update.workout')
   updateWorkout(@Payload() payload: { id: number, updateWorkoutDto: UpdateWorkoutDto }) {
-  //@Patch('update/:id')
-  //updateWorkout(@Param('id',ParseIntPipe)id:number,@Body() updateWorkoutDto: UpdateWorkoutDto){
     const {id,updateWorkoutDto}=payload
     return this.workoutService.updateWorkout(id, updateWorkoutDto);
   }
 
   @MessagePattern('remove.workout')
   remove(@Payload('id',ParseIntPipe) id: number) {
-  //@Delete('delete/:id')
-  //removeWorkout(@Param('id',ParseIntPipe) id: number) {
     return this.workoutService.removeWorkout(id);
   }
 }
