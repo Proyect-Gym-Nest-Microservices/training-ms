dd895467df0bf0f4cd2320013a5e290fabce15f2
sgualotuna <sgualotuna@cobiscorp.com>
2024-11-13 14:03:52 -0500

    model equipment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { ExerciseModule } from './exercise/exercise.module';
 import { WorkoutModule } from './workout/workout.module';
 import { TrainingPlanModule } from './training-plan/training-plan.module';
 import { MuscleGroupModule } from './muscle-group/muscle-group.module';
+import { EquipmentModule } from './equipment/equipment.module';
 
 @Module({
-  imports: [ExerciseModule, WorkoutModule, TrainingPlanModule, MuscleGroupModule],
+  imports: [ExerciseModule, WorkoutModule, TrainingPlanModule, MuscleGroupModule, EquipmentModule],
 })
 export class AppModule {}
